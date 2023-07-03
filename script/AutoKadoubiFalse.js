a293273e8c60eb1342f6c6f658e1d0916873dd73
anzai-moriQ <anzai130.yukiyuki@gmail.com>
2023-07-03 19:31:42 +0900

    稼働日を無しにするスクリプト

---
+++ script/AutoKadoubiFalse.js
@@ -0,0 +1,17 @@
+// 業務報告書の稼働日が〇の部分を無しにするJS
+for(let x = 1; x <= 31; x++){
+    let date;
+    let keta = (x + '').length;
+    if (keta < 2) {
+        let f = "0" + x;
+        date = "#dispatchDay" + f;
+    } else {
+        date = "#dispatchDay" + x;
+    }
+
+    let a = document.querySelector(date);
+    if(a.selectedIndex == "1"){
+        a.value = "0";
+    }
+    console.log(x + "件目の実行");
+}
